0b558cb02dcf5c1a68b7853720e94b71b9ceeab5
LeMinhDuc47 <fmgduc47@gmail.com>
2026-02-03 22:44:44 +0700

    feat(error): add global exception handling for IdInvalidException

--- src/main/java/com/minhduc/smartrestaurant/controller/UserController.java
+++ src/main/java/com/minhduc/smartrestaurant/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.minhduc.smartrestaurant.domain.User;
 import com.minhduc.smartrestaurant.service.UserService;
+import com.minhduc.smartrestaurant.service.error.IdInvalidException;
+
+import io.micrometer.core.instrument.Meter.Id;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
---
+++ src/main/java/com/minhduc/smartrestaurant/service/error/IdInvalidException.java
@@ -0,0 +1,8 @@
+package com.minhduc.smartrestaurant.service.error;
+
+public class IdInvalidException extends Exception {
+    public IdInvalidException(String message) {
+        super(message);
+    }
+
+}

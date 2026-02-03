55b10bceda22508e703e101ba84eccfb6d45f502
LeMinhDuc47 <fmgduc47@gmail.com>
2026-02-03 21:21:20 +0700

    feat(user): implement CRUD operations for user management

--- src/main/java/com/minhduc/smartrestaurant/controller/UserController.java
+++ src/main/java/com/minhduc/smartrestaurant/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.minhduc.smartrestaurant.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.minhduc.smartrestaurant.domain.User;
 import com.minhduc.smartrestaurant.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class UserController {
@@ -15,12 +23,30 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public String createUser() {
-        User user = new User();
-        user.setEmail("fmgduc79@gmail.com");
-        user.setName("Minh Duc");
-        user.setPassword("password123");
-        this.userService.handleCreateUser(user);
-        return "Create User";
+    public User createUser(@RequestBody User userRequest) {
+        User newUser = this.userService.handleCreateUser(userRequest);
+        return newUser;
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable("id") long id) {
+        return this.userService.fetchUserById(id);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUser() {
+        return this.userService.fetchAllUsers();
+    }
+
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User user) {
+        // TODO: process PUT request
+        User updateUser = this.userService.handleUpdateUser(user);
+        return updateUser;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
     }
 }
--- src/main/java/com/minhduc/smartrestaurant/service/UserService.java
+++ src/main/java/com/minhduc/smartrestaurant/service/UserService.java
@@ -1,5 +1,8 @@
 package com.minhduc.smartrestaurant.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.minhduc.smartrestaurant.domain.User;
@@ -13,8 +16,37 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void handleCreateUser(User user) {
+    public User handleCreateUser(User user) {
         // Logic to handle user creation
-        this.userRepository.save(user);
+        return this.userRepository.save(user);
+    }
+
+    public User fetchUserById(long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            throw new RuntimeException("User not found");
+        }
+    }
+
+    public List<User> fetchAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public User handleUpdateUser(User userDetails) {
+        // Logic to handle user update
+        User user = fetchUserById(userDetails.getId());
+        user.setName(userDetails.getName());
+        user.setEmail(userDetails.getEmail());
+        user.setPassword(userDetails.getPassword());
+        user = this.userRepository.save(user);
+
+        return user;
+    }
+
+    public void handleDeleteUser(long id) {
+        // Logic to handle user deletion
+        this.userRepository.deleteById(id);
     }
 }

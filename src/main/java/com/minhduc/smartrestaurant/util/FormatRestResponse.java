16602db10901179620233a64a4f7dbf707c6ba23
LeMinhDuc47 <fmgduc47@gmail.com>
2026-02-03 23:07:20 +0700

    feat(response): implement RestResponse class and format response handling

---
+++ src/main/java/com/minhduc/smartrestaurant/util/FormatRestResponse.java
@@ -0,0 +1,50 @@
+package com.minhduc.smartrestaurant.util;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import com.minhduc.smartrestaurant.domain.RestResponse;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@ControllerAdvice
+public class FormatRestResponse implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @Override
+    @Nullable
+    public Object beforeBodyWrite(@Nullable Object body, MethodParameter returnType, MediaType selectedContentType,
+            Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        // TODO Auto-generated method stub
+        HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+        int status = servletResponse.getStatus();
+
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(status);
+        // Handle Exception: RestResponse cannot be cast to class String
+        if (body instanceof String) {
+            return body;
+        }
+
+        if (status >= 400) {
+            return body;
+        } else {
+            // case success
+            res.setData(body);
+            res.setMessage("Call api success");
+        }
+
+        return res;
+    }
+
+}

ff60c7e7e17329d9cb778be0b6f00a15f79a55da
LeMinhDuc47 <fmgduc47@gmail.com>
2026-02-03 10:40:06 +0700

    init project

---
+++ src/main/java/com/minhduc/smartrestaurant/SmartRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.minhduc.smartrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minhduc/smartrestaurant/SmartRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhduc.smartrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

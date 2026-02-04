32aae72e411b4f03d35d3655a765cf81e51251a5
LeMinhDuc47 <fmgduc47@gmail.com>
2026-02-04 14:40:13 +0700

    feat(config): add DateTimeFormatConfiguration for ISO 8601 date formatting

---
+++ src/main/java/com/minhduc/smartrestaurant/config/DateTimeFormatConfiguration.java
@@ -0,0 +1,21 @@
+package com.minhduc.smartrestaurant.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configure the converters to use the ISO: 8601 format for dates by default.
+ */
+@Configuration
+public class DateTimeFormatConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        registrar.setUseIsoFormat(true);
+        registrar.registerFormatters(registry);
+    }
+
+}

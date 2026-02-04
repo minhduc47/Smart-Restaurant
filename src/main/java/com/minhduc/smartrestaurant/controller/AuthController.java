89238dda34afb17530ae8a72a4cb3889da439f24
LeMinhDuc47 <fmgduc47@gmail.com>
2026-02-04 13:54:23 +0700

    feat(security): implement custom authentication entry point and enhance security configuration

--- src/main/java/com/minhduc/smartrestaurant/controller/AuthController.java
+++ src/main/java/com/minhduc/smartrestaurant/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,8 @@ public class AuthController {
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
         // create token
         String access_token = this.securityUtil.createToken(authentication);
+        // Set information to SecurityContextHolder
+        SecurityContextHolder.getContext().setAuthentication(authentication);
         // Format response access_token
         ResLoginDTO res = new ResLoginDTO();
         res.setAccessToken(access_token);

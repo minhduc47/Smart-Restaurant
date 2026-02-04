cde8c59ad608549372f909967cda740439246da4
LeMinhDuc47 <fmgduc47@gmail.com>
2026-02-04 11:39:17 +0700

    feat(auth): implement JWT authentication with token generation and response DTO

--- src/main/java/com/minhduc/smartrestaurant/controller/AuthController.java
+++ src/main/java/com/minhduc/smartrestaurant/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.minhduc.smartrestaurant.controller;
 
+import java.security.Security;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,27 +11,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.minhduc.smartrestaurant.domain.dto.LoginDTO;
-
+import com.minhduc.smartrestaurant.domain.dto.ResLoginDTO;
+import com.minhduc.smartrestaurant.util.SecurityUtil;
 import jakarta.validation.Valid;
 
 @RestController
 public class AuthController {
 
+    private final SecurityUtil securityUtil;
+
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder) {
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil) {
+        this.securityUtil = securityUtil;
         this.authenticationManagerBuilder = authenticationManagerBuilder;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDTO> login(@Valid @RequestBody LoginDTO loginDTO) {
+    public ResponseEntity<ResLoginDTO> login(@Valid @RequestBody LoginDTO loginDTO) {
         // Nạp input gồm username/password vào Security
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 loginDTO.getUsername(), loginDTO.getPassword());
 
         // xác thực người dùng => cần viết hàm loadUserByUsername
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        // create token
+        String access_token = this.securityUtil.createToken(authentication);
+        // Format response access_token
+        ResLoginDTO res = new ResLoginDTO();
+        res.setAccessToken(access_token);
 
-        return ResponseEntity.ok().body(loginDTO);
+        return ResponseEntity.ok().body(res);
     }
 }

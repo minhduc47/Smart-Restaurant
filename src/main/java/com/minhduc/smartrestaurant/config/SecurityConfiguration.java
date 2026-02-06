7329658ebf5051c380870a0f35fb749851b74cb8
LeMinhDuc47 <fmgduc47@gmail.com>
2026-02-06 11:44:44 +0700

    feat(auth): implement refresh token functionality

--- src/main/java/com/minhduc/smartrestaurant/config/SecurityConfiguration.java
+++ src/main/java/com/minhduc/smartrestaurant/config/SecurityConfiguration.java
@@ -45,7 +45,7 @@ public class SecurityConfiguration {
                 .csrf(c -> c.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/api/v1/auth/login").permitAll()
+                        .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
                         .authenticationEntryPoint(customAuthenticationEntryPoint))

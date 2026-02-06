0b8934f315d5c3ecc65bc692e8ae5781faae2cd4
LeMinhDuc47 <fmgduc47@gmail.com>
2026-02-06 12:19:16 +0700

    refactor: customUserDetails

--- src/main/java/com/minhduc/smartrestaurant/config/CustomUserDetailsService.java
+++ src/main/java/com/minhduc/smartrestaurant/config/CustomUserDetailsService.java
@@ -1,4 +1,4 @@
-package com.minhduc.smartrestaurant.service;
+package com.minhduc.smartrestaurant.config;
 
 import java.util.Collections;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -7,6 +7,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import com.minhduc.smartrestaurant.service.UserService;
 
 @Component("userDetailsService")
 public class CustomUserDetailsService implements UserDetailsService {

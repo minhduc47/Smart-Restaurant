9c5adf683940ac11e30f1a81d7a113252ae27486
LeMinhDuc47 <fmgduc47@gmail.com>
2026-02-06 23:21:09 +0700

    feat(download): implement file download functionality

--- src/main/java/com/minhduc/smartrestaurant/service/FileService.java
+++ src/main/java/com/minhduc/smartrestaurant/service/FileService.java
@@ -1,6 +1,8 @@
 package com.minhduc.smartrestaurant.service;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -10,6 +12,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -43,6 +46,28 @@ public class FileService {
                     StandardCopyOption.REPLACE_EXISTING);
         }
         return finalName;
+    }
+
+    public long getFileLength(String fileName, String folder) throws URISyntaxException {
+        URI uri = new URI(folder + "/" + fileName);
+        Path path = Paths.get(uri);
+
+        File tmpDir = new File(path.toString());
+
+        // file not exist or file is 1 directory (thư mục thì sẽ ko có kích thước tệp)
+        if (!tmpDir.exists() || tmpDir.isDirectory()) {
+            return 0;
+        }
+        return tmpDir.length();
+    }
+
+    public InputStreamResource getResource(String fileName, String folder)
+            throws URISyntaxException, FileNotFoundException {
+        URI uri = new URI(folder + "/" + fileName);
+        Path path = Paths.get(uri);
+
+        File file = new File(path.toString());
+        return new InputStreamResource(new FileInputStream(file));
     }
 
 }

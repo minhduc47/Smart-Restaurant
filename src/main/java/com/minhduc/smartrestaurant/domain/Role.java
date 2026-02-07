42778fa747b23f9153d4ee63a9b89560f26ad8cd
LeMinhDuc47 <fmgduc47@gmail.com>
2026-02-07 10:37:07 +0700

    feat(role): add user-role relationship in Role and User entities

--- src/main/java/com/minhduc/smartrestaurant/domain/Role.java
+++ src/main/java/com/minhduc/smartrestaurant/domain/Role.java
@@ -3,16 +3,19 @@ package com.minhduc.smartrestaurant.domain;
 import java.time.Instant;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.minhduc.smartrestaurant.util.SecurityUtil;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -43,6 +46,9 @@ public class Role {
     @JsonIgnoreProperties(value = { "roles" })
     @JoinTable(name = "permission_role", joinColumns = @JoinColumn(name = "role_id"), inverseJoinColumns = @JoinColumn(name = "permission_id"))
     private List<Permission> permissions;
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<User> user;
 
     @PrePersist
     public void handleBeforeCreate() {
--- src/main/java/com/minhduc/smartrestaurant/domain/User.java
+++ src/main/java/com/minhduc/smartrestaurant/domain/User.java
@@ -12,6 +12,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -37,6 +39,10 @@ public class User {
     private GenderEnum gender;
     private String address;
     @Column(columnDefinition = "MEDIUMTEXT")
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
     private String refreshToken;
     private Instant createdAt;
     private Instant updatedAt;

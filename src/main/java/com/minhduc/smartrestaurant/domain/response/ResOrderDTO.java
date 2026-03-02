a442c1b880de7221730a66be86caf6d1f1b2cc1c
LeMinhDuc47 <fmgduc47@gmail.com>
2026-03-02 23:55:21 +0700

    feat(order): add ResOrderDTO for order response representation

---
+++ src/main/java/com/minhduc/smartrestaurant/domain/response/ResOrderDTO.java
@@ -0,0 +1,21 @@
+package com.minhduc.smartrestaurant.domain.response;
+
+import java.time.Instant;
+
+import com.minhduc.smartrestaurant.util.constant.OrderEnum;
+import com.minhduc.smartrestaurant.util.constant.OrderStatusEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResOrderDTO {
+    private long id;
+    private long totalPrice;
+    private OrderStatusEnum status;
+    private OrderEnum orderType;
+    private String note;
+    private Instant createdAt;
+
+    private Long tableId;
+}

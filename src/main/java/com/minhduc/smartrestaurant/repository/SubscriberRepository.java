61d485342200cdbb8310beb5fa10ee6fcc53d4f0
LeMinhDuc47 <fmgduc47@gmail.com>
2026-03-18 11:45:00 +0700

    feat(subscriber): add missing import for List in SubscriberRepository

--- src/main/java/com/minhduc/smartrestaurant/repository/SubscriberRepository.java
+++ src/main/java/com/minhduc/smartrestaurant/repository/SubscriberRepository.java
@@ -1,5 +1,7 @@
 package com.minhduc.smartrestaurant.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
